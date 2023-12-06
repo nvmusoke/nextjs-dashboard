02a0feff08495d7e8fb6fa6ff5fecca2b96d36a5
Namanda Musoke <nmusoke@vertify.com>
2023-12-06 15:06:34 -0600

    delete throw

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -73,7 +73,6 @@ catch (error) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');

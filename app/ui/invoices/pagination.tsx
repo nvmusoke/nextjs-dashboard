f140fd9be21ab13de2a987587e7fc112839ddd13
Namanda Musoke <nmusoke@vertify.com>
2023-12-14 18:59:22 -0600

    chapter 14

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,11 +12,11 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
-  const createPageURL = (pageNumber: number | string ) => {
+  const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
-  }
+  };
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}

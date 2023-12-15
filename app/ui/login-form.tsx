1d50bb906abf7013db6ba726d20bb1efad49b55c
Namanda Musoke <nmusoke@vertify.com>
2023-12-14 19:50:15 -0600

    form status

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -80,7 +80,7 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
-  const { pending } = useFormState();
+  const { pending } = useFormStatus();
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
